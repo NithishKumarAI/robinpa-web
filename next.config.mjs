0720de5bcddbe07784e0fa188eb6da1e2f50d3f3
Nithish Kumar R <nithishswim7@gmail.com>
2026-09-16 15:03:19 +0530

    chore: configure Robin for Firebase Hosting

--- next.config.mjs
+++ next.config.mjs
@@ -2,31 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-          {
-            key: "Permissions-Policy",
-            value: "camera=(), microphone=(), geolocation=(), browsing-topics=()",
-          },
-        ],
-      },
-    ];
-  },
+  output: "export",
 };
 
 export default nextConfig;
